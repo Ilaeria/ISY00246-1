5d0e69b220bf8bf492a60fa0b683ee196fc826fe
Jen Doherty <dohertyja@gmail.com>
2015-08-08 14:26:39 +1000

    Initial commit with contributors

---
+++ Assignment 1 Part 1/src/com/Part1/Part1.java
@@ -0,0 +1,37 @@
+/**
+ * Created by: Jennifer Doherty
+ * Unit: ISY00246 S2 2015
+ * Date: 21 August 2015
+ */
+
+package com.Part1;
+
+import java.io.*;
+
+public class Part1
+{
+
+    public static void main(String[] args)
+    {
+	    InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader stdin = new BufferedReader(isr);
+
+        int [] numbers = new int[10];
+
+        System.out.println("Enter two integers:");
+
+        try
+        {
+            String str = stdin.readLine();
+            int n = Integer.parseInt(str);
+            numbers[0] = n;
+            System.out.println(numbers[0]);
+        }
+        catch (Exception e)
+        {
+            System.out.println("An error occurred");
+        }
+
+
+    }
+}
