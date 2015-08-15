daf9ed20e7b878fc2d71d56b224e794ec51eaf24
Jen Doherty <dohertyja@gmail.com>
2015-08-15 18:25:19 +1000

    Part 3b initial commit

---
+++ Assignment 1 Part 3b/Assignment 1 Part 3b Server/src/com/Part3bServer/NameServer.java
@@ -0,0 +1,20 @@
+/**
+ * Created by: Jennifer Doherty
+ * Unit: ISY00246 S2 2015
+ * Date: 21 August 2015
+ * Assignment 1 Part 3a
+ * Sends vote info to the server program.
+ **/
+
+package com.Part3bServer;
+
+import java.io.*;
+import java.net.*;
+
+public class NameServer
+{
+    public static void main(String[] args)
+    {
+	// write your code here
+    }
+}
